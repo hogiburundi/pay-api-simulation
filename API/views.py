e08f2373652c404dddba691720dcc63ebc84c99a
HOGI Burundi <hogiburundi@gmail.com>
2019-09-05 17:45:03 +0200

    premiere version de l'API simulée

--- API/views.py
+++ API/views.py
@@ -4,20 +4,20 @@ from .models import InfoTransaction
 
 def check(request, id_transaction):
     transaction = InfoTransaction.objects.get(serie=id_transaction)
-
+    donnees = []
     if transaction:
         data = {
 			"nom" : transaction.nom,
 			"prenom" : transaction.prenom,
-			"date" : transaction.date,
+			"date" : str(transaction.date),
 			"source" : transaction.source,
 			"destination" : transaction.destination,
 			"montant" : transaction.montant,
 			"objet" : transaction.objet,
 			"serie" : transaction.serie,
         }
-        utilisateur.append(data)
-        json = jsonify(utilisateur)
+        donnees.append(data)
+        json = jsonify(donnees)
         return HttpResponse(json , content_type="application/json")
     return HttpResponse(None, content_type="application/json")
 
